3a05dae624b3a437a4cb511a2809cffa785fd451
Steve Snyder <snyder13@purdue.edu>
2018-12-07 16:51:20 -0500

    export directives

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const { html } = require('@popeindustries/lit-html-server');
+const directives = require('@popeindustries/lit-html-server/directives');
 
 module.exports = {
-	html
+	html,
+	...directives
 };
